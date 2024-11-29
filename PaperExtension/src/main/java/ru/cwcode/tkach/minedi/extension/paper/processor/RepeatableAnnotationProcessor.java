d17c45acb3c8e8243f478f3d9fae7e551889056b
Danil Tkach <tkach@cwcode.ru>
2024-11-29 20:41:38 +0300

    WIP Paper use MethodCaller instead of reflection in Repeat annotation process

--- PaperExtension/src/main/java/ru/cwcode/tkach/minedi/extension/paper/processor/RepeatableAnnotationProcessor.java
+++ PaperExtension/src/main/java/ru/cwcode/tkach/minedi/extension/paper/processor/RepeatableAnnotationProcessor.java
@@ -1,14 +1,13 @@
 package ru.cwcode.tkach.minedi.extension.paper.processor;
 
+import revxrsal.asm.BoundMethodCaller;
+import revxrsal.asm.MethodCaller;
 import ru.cwcode.cwutils.scheduler.Scheduler;
 import ru.cwcode.cwutils.scheduler.annotationRepeatable.Repeat;
 import ru.cwcode.tkach.minedi.DiApplication;
 import ru.cwcode.tkach.minedi.extension.paper.PaperExtension;
 import ru.cwcode.tkach.minedi.processing.event.CustomMethodAnnotationEvent;
 import ru.cwcode.tkach.minedi.processing.processor.EventProcessor;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class RepeatableAnnotationProcessor extends EventProcessor<CustomMethodAnnotationEvent> {
   private final PaperExtension extension;
@@ -23,20 +22,13 @@ public class RepeatableAnnotationProcessor extends EventProcessor<CustomMethodAn
     if (!event.getAnnotation().annotationType().equals(Repeat.class)) return;
     Repeat annotation = (Repeat) event.getAnnotation();
     
-    Method method = event.getMethod();
-    method.setAccessible(true);
-    extension.addDelayedTask(() -> {
-      Scheduler.create()
-               .async(annotation.async())
-               .infinite()
-               .perform(() -> {
-                 try {
-                   method.invoke(event.getBean());
-                 } catch (IllegalAccessException | InvocationTargetException e) {
-                   throw new RuntimeException(e);
-                 }
-               })
-               .register(extension.getPlugin(), annotation.delay());
-    });
+    BoundMethodCaller boundMethodCaller = MethodCaller.wrap(event.getMethod())
+                                                      .bindTo(event.getBean());
+    
+    extension.addDelayedTask(() -> Scheduler.create()
+                                          .async(annotation.async())
+                                          .infinite()
+                                          .perform(boundMethodCaller::call)
+                                          .register(extension.getPlugin(), annotation.delay()));
   }
 }
